4e4860e5534ba0bac20290971796bf03e4f2e4b1
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-21 13:44:24 +0530

    Add call to database
    - Added lines to write to input value to a file kv.db
    - Added lines to construct a database and use the Database struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+pub(crate) mod database;
+
 fn main() {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().unwrap();
     let value = arguments.next().unwrap();
 
-    println!("The key is '{}' and value is '{}'", key, value);
+    println!("Input -> key='{}' and value='{}'", key, value);
+
+    let data = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", data).unwrap();
+
+    let _db = database::Database::new().expect("database creation crashed!");
+
+    //println!("{}", db::map);
 }
