cb561da27ceab2bd129200d4bcc6e906e2320b94
Manish Meganathan <meganathan.manish@gmail.com>
2021-06-24 18:18:15 +0530

    Update main.rs
    - Commented the db related code
    - Added a simple REPL interface for FerroQL

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
-pub(crate) mod database;
+mod database;
+mod ferroql;
+
+use std::io::{self, Write};
+
 
 fn main() {
-    let mut arguments = std::env::args().skip(1);
-    let key = arguments.next().unwrap();
-    let value = arguments.next().unwrap();
-
-    println!("Input -> key='{}' and value='{}'", key, value);
-
-    let mut db = database::Database::new().expect("database creation crashed!");
-
-    db.insert(key.clone(), value.clone());
-
+    // let mut arguments = std::env::args().skip(1);
+    // let key = arguments.next().unwrap();
+    // let value = arguments.next().unwrap();
+    // println!("Input -> key='{}' and value='{}'", key, value);
+    // let mut db = database::Database::new().expect("database creation crashed!");
+    // db.insert(key.clone(), value.clone());
     //db.flush().unwrap();
+
+    loop {
+        print!("ferroQL>: ");
+        io::stdout().flush().unwrap();
+        
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("failed to read input");
+
+        let mut l = ferroql::lexer::Lexer::new(input.chars().collect());
+        let tokens = l.lex();
+
+        for token in tokens.into_iter() {
+            println!("{}", token);
+        }
+    }    
 }
