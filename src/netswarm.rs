60f706e6ae9d680a811ef04471ee15d729cef2c8
Manish Meganathan <meganathan.manish@gmail.com>
2021-07-19 00:42:42 +0530

    Add swarm construction to setup_swarm
    - Added code that sets up and creates the swarm with the previously defined identity and transport parameters.

--- src/netswarm.rs
+++ src/netswarm.rs
@@ -1,10 +1,18 @@
 // This module implements functionality for a p2p network swarm.
 
-use libp2p::{
-    identity,
-    development_transport
+use async_std::task;
+use libp2p::{identity, development_transport};
+use libp2p::mdns::{Mdns, MdnsConfig};
+use libp2p::kad::record::store::MemoryStore;
+use libp2p::kad::{
+    Kademlia
 };
-use libp2p::PeerId;
+use libp2p::{
+    PeerId, 
+    Swarm
+};
+
+use crate::netbehaviour::Behaviour;
 
 pub async fn setup_swarm() {
     // Create a random keypair from the secp256k1 curve.
@@ -15,4 +23,19 @@ pub async fn setup_swarm() {
     // Set up a standard P2P transport layer.
     let transport = development_transport(keypair).await?;
 
+    // Create a swarm to manage peers and events.
+    let mut swarm = {
+        // Create a Memory Store
+        let store = MemoryStore::new(peerid);
+        // Setup the Memory Store with Kademlia 
+        let kademlia = Kademlia::new(peerid, store);
+
+        // Create the mDNS discovery service for the swarm
+        let mdns = task::block_on(Mdns::new(MdnsConfig::default()))?;
+        // Create the network behaviour for the swarm
+        let behaviour = Behaviour { kademlia, mdns };
+
+        // Create the swarm
+        Swarm::new(transport, behaviour, peerid)
+    };
 }
